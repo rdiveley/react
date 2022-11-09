5c46b1f389a787562bbf700006cd24c5e4c67858
Ray Diveley <rdiveley@wellcoaches.com>
2022-11-09 14:31:18 -0500

    cleaning up app.js

--- react/demo/src/App.js
+++ react/demo/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        <p>
-          Hello!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Employee name="Ray" />
+        
       </header>
     </div>
   );
---
+++ react/demo/src/components/Employee.js
@@ -0,0 +1,4 @@
+function Employee(props) { 
+    return <h3>Here is { props.name}! </h3>
+}
+export default Employee;
