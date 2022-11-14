fc661cee86815fc591c09f89906ddf37b4d830d7
Ray Diveley <rdiveley@wellcoaches.com>
2022-11-14 07:54:44 -0500

    added onChange

---
+++ react/demo/src/components/EditEmployee.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function EditEmployee(props) {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+          <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+        Update
+      </button>
+      
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         
+            <form id='editmodal' className="w-full max-w-sm">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Full Name
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        id="name" 
+                        type="text" 
+                        placeholder="Full Name"
+                        value={name}
+                        onChange={(e)=>{
+                            setName(e.target.value)
+                        }}
+                         />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="role">
+                        Role
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        id="role" 
+                        type="text" 
+                        placeholder="Role" 
+                        value={role}
+                        onChange={(e)=>{
+                            setRole(e.target.value)
+                        }}
+                         />
+                </div>
+               
+            </form>
+        </Modal.Body>
+        <Modal.Footer>
+          
+          <button className='bg-slate-400 hover:bg-slate-400 text-white font-bold py-2 px-4 rounded' onClick={handleClose} >Close</button>
+          <button className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded' form='editmodal' >Update</button>
+          
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default EditEmployee;
--- react/demo/src/components/Employee.js
+++ react/demo/src/components/Employee.js
@@ -1,6 +1,8 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) { 
     return (
-      <div className="m-2 py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img className="object-cover rounded-full h-[100] w-[100] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
           src={props.img}/>
         <div className="text-center space-y-2 sm:text-left">
@@ -12,7 +14,9 @@ function Employee(props) {
                     { props.role}
             </p>
           </div>
-          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
+
+          <EditEmployee name={props.name} role={props.role} />
+
         </div>
       </div>
     );
